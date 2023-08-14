a997d50f014143d33a0328a983b47b51a6dd60d3
zh4men9 <zh4men9@163.com>
2023-08-14 17:43:01 +0800

    add new shell scripts

---
+++ 4-install_miniconda.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# 定义 Miniconda 版本和安装路径
+miniconda_version="latest"  # 或指定特定版本，如 "4.10.3"
+install_path="$HOME/miniconda"
+
+# 检查是否已安装 Miniconda
+if [ -d "$install_path" ]; then
+    echo "Miniconda 已经安装在 $install_path。跳过安装阶段。"
+else
+    # 下载 Miniconda 安装脚本
+    wget "https://repo.anaconda.com/miniconda/Miniconda3-$miniconda_version-Linux-x86_64.sh" -O miniconda_installer.sh
+
+    # 执行安装脚本
+    bash miniconda_installer.sh -b -p "$install_path"
+
+    # 添加 Miniconda 到 PATH
+    echo "export PATH=\"$install_path/bin:\$PATH\"" >> "$HOME/.bashrc"
+    source "$HOME/.bashrc"
+
+    # 清理安装文件
+    rm miniconda_installer.sh
+
+    echo "Miniconda 安装完成。请重新启动终端或运行 'source ~/.bashrc' 以使更改生效。"
+fi
+
+# 添加 Conda 到 PATH
+export PATH="$install_path/bin:$PATH"
+
+# 配置 Conda 清华源
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
+
+# 打印 Conda 源配置信息
+conda config --show-sources
+
+# 修改 .condarc 文件的内容
+cat << EOF > "$HOME/.condarc"
+channels:
+  - defaults
+show_channel_urls: true
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+EOF
+
+echo ".condarc 文件已更新为清华源配置。"
+
+# 打印 Conda 源配置信息
+conda config --show-sources
+
+echo "已将 Conda 源切换为清华源。"
---
+++ 5-install_cuda.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# 获取系统版本
+ubuntu_version=$(lsb_release -rs | cut -d. -f1)
+
+if [ "${ubuntu_version}" -ne 20 ] && [ "${ubuntu_version}" -ne 22 ]; then
+    echo "此脚本适用于 Ubuntu 20.04 和 22.04，请确认您的系统版本。"
+    exit 1
+fi
+
+# 下载并配置 CUDA 官方源
+if [ "${ubuntu_version}" -eq 20 ]; then
+    cuda_repo="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64"
+elif [ "${ubuntu_version}" -eq 22 ]; then
+    cuda_repo="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64"
+fi
+
+if [ ! -f "cuda-ubuntu${ubuntu_version}04.pin" ]; then
+    echo "下载并配置 CUDA 官方源..."
+    wget "$cuda_repo/cuda-ubuntu${ubuntu_version}04.pin"
+    sudo mv "cuda-ubuntu${ubuntu_version}04.pin" /etc/apt/preferences.d/cuda-repository-pin-600
+fi
+
+# 下载 CUDA 安装包
+cuda_package="cuda-repo-ubuntu${ubuntu_version}04-12-2-local_12.2.1-535.86.10-1_amd64.deb"
+if [ ! -f "$cuda_package" ]; then
+    echo "下载 CUDA 安装包..."
+    wget "$cuda_repo/12.2.1/local_installers/$cuda_package"
+fi
+
+# 安装 CUDA 安装包
+echo "安装 CUDA 安装包..."
+sudo dpkg -i "$cuda_package"
+
+# 复制密钥到 keyrings 目录
+echo "复制密钥到 keyrings 目录..."
+sudo cp /var/cuda-repo-ubuntu${ubuntu_version}04-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+# 更新软件源
+echo "更新软件源..."
+sudo apt-get update
+
+# # 安装cuda-drivers-535
+# sudo apt-get install nvidia-utils-535-server -y
+# sudo apt-get install cuda-drivers-535
+
+# 安装 CUDA
+echo "安装 CUDA..."
+sudo apt-get -y install cuda
+
+# 设置环境变量
+echo "设置环境变量..."
+echo "export PATH=/usr/local/cuda-12.2/bin:\$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+echo "CUDA 环境变量设置完成。"
+
+# 验证安装
+echo "验证 CUDA 安装..."
+nvcc --version
+nvidia-smi
+
+echo "CUDA 安装和验证完成。请重新启动终端以使环境生效。"
