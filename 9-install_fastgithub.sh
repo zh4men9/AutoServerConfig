a425d0a932db77fa6bd6b767ad676f16a28c49b1
zh4men9 <zh4men9@163.com>
2023-08-15 11:38:28 +0800

    update

--- 9-install_fastgithub.sh
+++ 9-install_fastgithub.sh
@@ -9,14 +9,20 @@ sudo ./fastgithub_linux-x64/fastgithub start
 proxy_url="http://127.0.0.1:38457"
 
 # 设置代理
-gsettings set org.gnome.system.proxy mode 'auto'
-gsettings set org.gnome.system.proxy autoconfig-url "$proxy_url"
+gsettings set org.gnome.system.proxy mode auto
+gsettings set org.gnome.system.proxy autoconfig-url $proxy_url
 
 export http_proxy=http://127.0.0.1:38457
 export https_proxy=http://127.0.0.1:38457
 
 git config --global http.proxy http://127.0.0.1:38457
 git config --global https.proxy http://127.0.0.1:38457
+
+git config --global --get http.proxy
+git config --global --get https.proxy
+
+gsettings get org.gnome.system.proxy mode
+gsettings get org.gnome.system.proxy autoconfig-url
 
 # 打印设置信息
 echo "已设置系统自动代理为: $proxy_url"
