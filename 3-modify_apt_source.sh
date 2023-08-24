416ba3c4ee6963f1373488631ce7d8ffdf9e94bb
zh4men9 <zh4men9@163.com>
2023-08-24 07:05:50 +0000

    update

---
+++ 3-modify_apt_source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 备份当前的 APT 源列表
+backup_file="/etc/apt/sources.list.backup"
+sudo cp /etc/apt/sources.list "$backup_file"
+
+# 替换 APT 源为清华源
+sudo tee /etc/apt/sources.list << EOF
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+EOF
+
+echo "已将 APT 源修改为清华源。"
