18f8ea3f1a6a51626ec84f0af2a3d1ccfa2b76c6
zh4men9 <zh4men9@163.com>
2023-08-24 10:01:56 +0000

    update cuda install

---
+++ 5-install_cuda12.2.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# 获取系统版本
+ubuntu_version=$(lsb_release -rs | cut -d. -f1)
+
+if [ "${ubuntu_version}" -ne 20 ] && [ "${ubuntu_version}" -ne 22 ]; then
+    echo "此脚本适用于 Ubuntu 20.04 和 22.04，请确认您的系统版本。"
+    exit 1
+fi
+
+# 下载并配置 CUDA 官方源
+if [ "${ubuntu_version}" -eq 20 ]; then
+    cuda_repo="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64"
+elif [ "${ubuntu_version}" -eq 22 ]; then
+    cuda_repo="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64"
+fi
+
+if [ ! -f "cuda-ubuntu${ubuntu_version}04.pin" ]; then
+    echo "下载并配置 CUDA 官方源..."
+    wget "$cuda_repo/cuda-ubuntu${ubuntu_version}04.pin"
+    sudo mv "cuda-ubuntu${ubuntu_version}04.pin" /etc/apt/preferences.d/cuda-repository-pin-600
+fi
+
+# 下载 CUDA 安装包
+cuda_package="cuda-repo-ubuntu${ubuntu_version}04-12-2-local_12.2.1-535.86.10-1_amd64.deb"
+if [ ! -f "$cuda_package" ]; then
+    echo "下载 CUDA 安装包..."
+    wget "https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/$cuda_package"
+fi
+
+# 安装 CUDA 安装包
+echo "安装 CUDA 安装包..."
+sudo dpkg -i "$cuda_package"
+
+# 复制密钥到 keyrings 目录
+echo "复制密钥到 keyrings 目录..."
+sudo cp /var/cuda-repo-ubuntu${ubuntu_version}04-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+# 更新软件源
+echo "更新软件源..."
+sudo apt-get update
+
+# 安装 CUDA
+echo "安装 CUDA..."
+sudo apt-get -y install cuda
+sudo apt -y install nvidia-cuda-toolkit
+
+# 设置环境变量
+echo "设置环境变量..."
+echo "export PATH=/usr/local/cuda-12.2/bin:\$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-12.2/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+echo "CUDA 环境变量设置完成。"
+
+# sudo apt-get install dkms
+# sudo dkms install -m nvidia -v 515.43.04
+
+# # 安装cuda-drivers-535
+# 安装驱动的时候可能需要手动安装
+echo "安装驱动..."
+# sudo apt install ubuntu-drivers-common -y
+# ubuntu-drivers devices
+# sudo apt install nvidia-driver-535 -y
+# sudo apt install -y nvidia-utils-535-server
+# sudo apt install libnvidia-compute-535-server
+sudo apt update
+sudo apt install ubuntu-drivers-common -y
+sudo apt install libnvidia-common-535-server -y
+sudo ubuntu-drivers autoinstall
+
+# 验证安装
+echo "验证 CUDA 安装..."
+nvcc --version
+nvidia-smi
+
+echo "CUDA 安装和验证完成。请重新启动终端以使环境生效。"
+
+
+read -p "是否删除$cuda_package (y/n): " confirm
+
+if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
+    echo "操作已取消。"
+else
+    rm -rf $cuda_package
+fi
+
+
+read -p "需要重启生效，确定重启？ (y/n): " confirm
+
+if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
+    echo "操作已取消。"
+    exit 1
+fi
+
+# 重启系统
+echo "系统将在5秒后重启..."
+sleep 5
+sudo reboot
